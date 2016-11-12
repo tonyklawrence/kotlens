1d1b90412320b46065b6487e834ee9823e4ac024
Tony Lawrence <code@tonylawrence.com>
2016-11-12 17:39:56 +0000

    add modify, reverse and combine to Iso - to be implemented later

--- src/main/kotlin/org/evilducks/kotlens/Iso.kt
+++ src/main/kotlin/org/evilducks/kotlens/Iso.kt
@@ -1,3 +1,9 @@
 package org.evilducks.kotlens
 
-data class Iso<S, A>(val get: (S) -> A, val reverseGet: (A) -> S)
+data class Iso<S, A>(val get: (S) -> A, val reverseGet: (A) -> S) {
+    fun modify(m: (A) -> A): (S) -> S = notImplemented()
+    fun reverse(): Iso<A, S> = notImplemented()
+    fun <B> combine(other: Iso<A, B>): Iso<S, B> = notImplemented()
+}
+
+fun Any.notImplemented(): Nothing = throw NotImplementedError(this.toString())
