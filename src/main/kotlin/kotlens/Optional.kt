2dd2bc9738e0effd96aa9910ec09afd9a4d28d87
Tony Lawrence <code@tonylawrence.com>
2017-02-25 09:12:33 +0000

    test index / at in optionals

--- src/main/kotlin/kotlens/Optional.kt
+++ src/main/kotlin/kotlens/Optional.kt
@@ -1,15 +1,20 @@
 package kotlens
 
-data class Optional<S, A>(val getOption: (S) -> A?, val set: (A, S) -> S) {
+data class Optional<S, A>(val getOption: (S) -> A?, val set: (A?, S) -> S) {
     fun modify(ƒ: (A) -> A): (S) -> S = { s -> modifyOption(ƒ)(s) ?: s }
     fun modifyOption(ƒ: (A) -> A): (S) -> S? = { s -> getOption(s)?.let { a -> set(ƒ(a), s) } }
+
     infix fun <B> compose(other: Optional<A, B>): Optional<S, B> = Optional(
             getOption = { s -> getOption(s)?.let { a -> other.getOption(a) } },
             set = { b, s -> getOption(s)?.let { a -> set(other.set(b, a), s) } ?: s }
     )
 
     companion object {
-        fun <A> index(i: Int) = Optional<List<A>, A>({ it.elementAtOrNull(i) }, { a, l -> l.mapIndexed { i2, a2 -> if (i == i2) a else a2 } })
-        fun <A, S> index(key: A) = Optional<Map<A, S>, S>({ it[key] }, { a, l -> l + (key to a) })
+        fun <A> index(i: Int) = Optional<List<A>, A>({ it.elementAtOrNull(i) }, { a, l ->
+            a?.let { l.mapIndexed { i2, a2 -> if (i == i2) a else a2 } } ?: l
+        })
+        fun <A, S> at(key: A) = Optional<Map<A, S>, S>({ it[key] }, { a, l ->
+            a?.let { l + (key to a) } ?: l.filterKeys { it != key }
+        })
     }
 }
--- src/test/kotlin/kotlens/OptionalTest.kt
+++ src/test/kotlin/kotlens/OptionalTest.kt
@@ -13,7 +13,7 @@ class OptionalTest {
     val mrTickle = Person("Mr Tickle", 34, tickleTown)
 
     @Test fun `optional properties`() {
-        val optional = Optional<Person, Int>({ it.age }, { age, person -> person.copy(age = age) })
+        val optional = Optional<Person, Int>({ it.age }, { age, person -> age?.let { person.copy(age = age) } ?: person })
 
         optional.getOption(mrTickle) shouldMatch equalTo(34)
         optional.set(100, mrTickle) shouldMatch equalTo(Person("Mr Tickle", 100, tickleTown))
@@ -21,7 +21,7 @@ class OptionalTest {
 
     @Test fun `composing optionals`() {
         val address = Optional<Person, Address>({ it.address }, { address, person -> person.copy(address = address) })
-        val street = Optional<Address, String>({ it.street }, { street, address -> address.copy(street = street) })
+        val street = Optional<Address, String>({ it.street }, { street, address -> street?.let { address.copy(street = street) } ?: address })
 
         val personToStreet = address compose street
         personToStreet.getOption(mrTickle) shouldMatch equalTo("Tickle Town")
@@ -30,5 +30,54 @@ class OptionalTest {
 
         address.getOption(homelessJoe) shouldMatch absent()
         personToStreet.getOption(homelessJoe) shouldMatch absent()
+    }
+
+    @Test fun `can access list using index`() {
+        val second = Optional.index<Int>(1)
+
+        second.getOption(emptyList()) shouldMatch absent()
+        second.getOption(listOf(1)) shouldMatch absent()
+        second.getOption(listOf(1, 2, 3)) shouldMatch equalTo(2)
+    }
+
+    @Test fun `can update a list using index`() {
+        val second = Optional.index<Int>(1)
+
+        second.set(0, emptyList()) shouldMatch equalTo(emptyList())
+        second.set(0, listOf(1)) shouldMatch equalTo(listOf(1))
+        second.set(0, listOf(1, 2, 3)) shouldMatch equalTo(listOf(1, 0, 3))
+    }
+
+    @Test fun `does not remove from index`() {
+        val second = Optional.index<Int>(1)
+
+        second.set(null, emptyList()) shouldMatch equalTo(emptyList())
+        second.set(null, listOf(1)) shouldMatch equalTo(listOf(1))
+        second.set(null, listOf(1, 2, 3)) shouldMatch equalTo(listOf(1, 2, 3))
+    }
+
+    @Test fun `can access map using at`() {
+        val name = Optional.at<String, String>("name")
+
+        name.getOption(emptyMap()) shouldMatch absent()
+        name.getOption(mapOf("foo" to "bar")) shouldMatch absent()
+        name.getOption(mapOf("name" to "value")) shouldMatch equalTo("value")
+    }
+
+    @Test fun `can update map using at`() {
+        val name = Optional.at<String, String>("name")
+
+        name.set("value", emptyMap()) shouldMatch equalTo(mapOf("name" to "value"))
+        name.set("value", mapOf("foo" to "bar")) shouldMatch equalTo(mapOf("foo" to "bar", "name" to "value"))
+        name.set("value", mapOf("name" to "bar")) shouldMatch equalTo(mapOf("name" to "value"))
+    }
+
+    @Test fun `can remove from map using at`() {
+        val name = Optional.at<String, String>("name")
+
+        name.set(null, emptyMap()) shouldMatch equalTo(emptyMap())
+        name.set(null, mapOf("foo" to "bar")) shouldMatch equalTo(mapOf("foo" to "bar"))
+        name.set(null, mapOf("name" to "value")) shouldMatch equalTo(emptyMap())
+        name.set(null, mapOf("foo" to "bar", "name" to "value")) shouldMatch equalTo(mapOf("foo" to "bar"))
     }
 }
