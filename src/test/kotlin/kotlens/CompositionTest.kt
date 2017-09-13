e14a1c8bd123950235e2a0a2cde93a5bf0178289
Tony Lawrence <code@tonylawrence.com>
2017-09-13 11:01:28 +0100

    Allow composition of lens with optionals via lens.toOptional

--- src/test/kotlin/kotlens/CompositionTest.kt
+++ src/test/kotlin/kotlens/CompositionTest.kt
@@ -19,4 +19,18 @@ class CompositionTest {
         isoToPrism.getOption("no!") shouldMatch absent()
         isoToPrism.reverseGet(800) shouldMatch equalTo("008")
     }
+
+    @Test fun `can compose lenses with other optics`() {
+        val lens = Lens<Data, List<Int>>({ it.numbers }, { numbers, data -> data.copy(numbers = numbers) })
+        val optional = Optional.index<Int>(2)
+
+        val lensToOptional = lens compose optional
+
+        lensToOptional.getOption(Data(listOf(2, 4, 6, 8, 10))) shouldMatch equalTo(6)
+        lensToOptional.getOption(Data(emptyList())) shouldMatch absent()
+        lensToOptional.set(9, Data(listOf(1, 2, 3, 4, 5))) shouldMatch equalTo(Data(listOf(1, 2, 9, 4, 5)))
+        lensToOptional.set(9, Data(emptyList())) shouldMatch equalTo(Data(emptyList()))
+    }
+
+    private data class Data(val numbers: List<Int>)
 }
