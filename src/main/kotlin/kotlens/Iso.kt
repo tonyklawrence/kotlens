310869f3ec530fc1409af330004fd136e6460a88
Tony Lawrence <code@tonylawrence.com>
2018-04-03 09:02:01 +0100

    Use funk composition in iso

--- src/main/kotlin/kotlens/Iso.kt
+++ src/main/kotlin/kotlens/Iso.kt
@@ -1,11 +1,13 @@
 package kotlens
 
+import funk.compose
+
 data class Iso<S, A>(val get: (S) -> A, val reverseGet: (A) -> S) {
     fun modify(f: (A) -> A): (S) -> S = { s -> reverseGet(f(get(s))) }
     fun reverse(): Iso<A, S> = Iso(reverseGet, get)
 
     infix fun <B> compose(other: Iso<A, B>): Iso<S, B> = Iso(
-            get = { s -> other.get(get(s)) },
-            reverseGet = { b -> reverseGet(other.reverseGet(b)) }
+            get = other.get compose get,
+            reverseGet = reverseGet compose other.reverseGet
     )
 }
--- src/test/kotlin/kotlens/examples/Isomorphisms.kt
+++ src/test/kotlin/kotlens/examples/Isomorphisms.kt
@@ -11,9 +11,9 @@ data class Kilometer(val value: Double)
 data class Mile(val value: Double)
 
 class Isomorphisms {
-    val meterToYard = Iso<Meter, Yard>({ Yard(it.value * 1.09361) }, { Meter(it.value / 1.09361) })
-    val meterToKm = Iso<Meter, Kilometer>({ Kilometer(it.value * 1000) }, { Meter(it.value / 1000) })
-    val yardToMile = Iso<Yard, Mile>({ Mile(it.value / 1760) }, { Yard(it.value * 1760) })
+    private val meterToYard = Iso<Meter, Yard>({ Yard(it.value * 1.09361) }, { Meter(it.value / 1.09361) })
+    private val meterToKm = Iso<Meter, Kilometer>({ Kilometer(it.value * 1000) }, { Meter(it.value / 1000) })
+    private val yardToMile = Iso<Yard, Mile>({ Mile(it.value / 1760) }, { Yard(it.value * 1760) })
 
     @Test fun `composing isomorphisms`() {
         val kmToMile = meterToKm.reverse() compose meterToYard compose yardToMile
